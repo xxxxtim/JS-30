202627cbc6afd89531914b507199b61dfc2b21e2
xxxxtim <xxxxtim@hotmail.com>
2020-04-16 01:55:03 +0800

    JS10-finish

--- 10 - Hold Shift and Check Checkboxes/index-START.js
+++ 10 - Hold Shift and Check Checkboxes/index-START.js
@@ -3,7 +3,7 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 // console.log(checkboxes);
 //設置一個變數let lastChecked;紀錄最後一個勾選
-let lastCheck;
+let lastChecked;
 
 //step 2
 //為每個checkbox加上click事件
@@ -11,10 +11,37 @@ checkboxes.forEach(function (checkbox) {
     return checkbox.addEventListener('click', handleCheck)
 })
 function handleCheck(event) {
-
+    //let inBetween = false來當作選取區間的標記
+    let inBetween = false;
     // this 指的是<input type="checkbox">
     // this.checked :判斷有無打勾 有的話回傳true
-    if (event.shiftKey && this.checked) { }
+    if (event.shiftKey && this.checked) {
+
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.log('Starting to check them in between!');
+            }
+            // 勾選區間內為true的checkbox
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        });
+
+        ////////////////////////////////////////////////////////////////
+        // checkboxes.forEach(function (checkbox) {
+        //     console.log(checkbox);
+        //     if (checkbox === this || checkbox === lastChecked) {
+        //         inBetween = !inBetween;
+        //         console.log('Starting to check them in between!');
+        //     }
+
+        //     if (inBetween) {
+        //         checkbox.checked = true;
+        //     }
+        // });
+    }
 
 
 }//function handleCheck
