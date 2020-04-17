09d6a80e989ac0de452fbf3ade83704ed30cc49d
xxxxtim <xxxxtim@hotmail.com>
2020-04-17 16:17:36 +0800

    JS11-finish

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,7 +5,7 @@ const player = document.getElementsByClassName('player')[0];
 const video = player.querySelector('.viewer');//影片
 // console.log(video);
 const progress = player.querySelector('.progress');// 進度條
-const progressBar = player.querySelector('.progress__filled');// 進度條
+const progressBar = player.querySelector('.progress__filled');// 顯示當前進度條
 const toggle = player.querySelector('.toggle'); // 暫停播放鍵
 const ranges = player.querySelectorAll('.player__slider'); // 聲音 速度
 const skipBtns = player.querySelectorAll('[data-skip]'); // 前後快轉
@@ -66,4 +66,38 @@ ranges.forEach(function (range) {
     return range.addEventListener('mousemove', handleRangeUpdate)
 })
 
-//進度條
+//進度條（顯示現在影片當前位置）
+function progressUpdate() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`
+}//function handleProgress
+video.addEventListener('progress', progressUpdate);
+
+//進度條調整
+function scrub(e) {
+    console.log(e);
+    // const scrubTime = (e.offsetx / progress.offsetwidth) * video.duration;
+    //（滑鼠移動的距離/整條移動bar的長度）*整個影片長度
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    //video.duration :表示影片長度（second）
+    video.currentTime = scrubTime;// 改變影片當前秒數
+    console.log(e.offsetX);
+    console.log(progress.offsetWidth);
+    console.log(video.duration)
+}//function scrub
+
+// 斷滑鼠是否點擊
+let mousedown = false;
+
+progress.addEventListener('click', scrub);
+// progress.addEventListener('mousemove', scrub);
+
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+//這行超屌
+//mousedown 只有true 和false 
+//true 和任何東西&& =任何東西
+//false 和任何東西 && = 0
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e))
+//有按下滑鼠 return scrube(e)=>執行
+//沒按下滑鼠  return 0  scrube(e)=>沒有執行
