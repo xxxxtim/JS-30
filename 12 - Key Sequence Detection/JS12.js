b25cc071cf7d17d74fc6de89a10140071ffb9bd5
xxxxtim <xxxxtim@hotmail.com>
2020-04-18 22:31:52 +0800

    JS13-FIFO method

--- 12 - Key Sequence Detection/JS12.js
+++ 12 - Key Sequence Detection/JS12.js
@@ -7,9 +7,14 @@ window.addEventListener('keyup', function (e) {
     pressed.push(e.key);
     // console.log(pressed)
     //透過運算使pressed陣列長度始終與設定密碼相同，且當超出時替換掉陣列第一個元素
-    console.log(secretCode.length);
-    pressed.splice((-secretCode.length) - 1, pressed.length - secretCode.length);
-    console.log(pressed)
+    //FIFO寫法1
+    // console.log(secretCode.length);
+    // pressed.splice((-secretCode.length) - 1, pressed.length - secretCode.length);
+    // console.log(pressed)
+    //FIFO寫法2
+    if (pressed.length > secretCode.length) {
+        pressed.shift();
+    }
     //判斷輸入值陣列的內容是否與設定密碼相同
     if (pressed.join('').includes(secretCode)) {
         console.log('Perfect!');
